9c0a2eb8e6d5cef73252ed6b1c2abda69b9e24c0
Kasir Abbas <kasir.abbas@ao.com>
2020-04-07 14:40:56 +0100

    First Publish
    Initial setup.
    added pettier
    added react
    added react-dom

---
+++ quiz-manager/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Initialise project: Quiz Manager by Kasir Abbas</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
